d0af73c331397c64180dea5b3dbf4cf5d4b802a8
Semo <semox78@gmail.com>
2012-05-07 12:45:07 +0200

    4 basic opertors including assertions working

--- main.cpp
+++ main.cpp
@@ -13,9 +13,15 @@ int main()
     RationalNumber a (2,3);
     RationalNumber b (5,3);
 
-    RationalNumber r (7,3);
+    RationalNumber r1 (7,3);
+    RationalNumber r2 (-3,3);
+    RationalNumber r3 (10,9);
+    RationalNumber r4 (2,5);
 
-    assert(r == a+b);
+    assert(r1 == a+b);
+    assert(r2 == a-b);
+    assert(r3 == a*b);
+    assert(r4 == a/b);
 
     cout << "Hello World!" << endl;
     return 0;
