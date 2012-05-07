0d3ff14a1d045b298ed327028fd7c6468c1ee402
Semo <semox78@gmail.com>
2012-05-07 12:29:36 +0200

    first refactoring plus assertion

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include "RationalNumber.h"
+#include <assert.h>
 
 using namespace std;
+using namespace rnum;
+
+
 
 int main()
 {
+
+    RationalNumber a (2,3);
+    RationalNumber b (5,3);
+
+    RationalNumber r (7,3);
+
+    assert(r == a+b);
+
     cout << "Hello World!" << endl;
     return 0;
 }
