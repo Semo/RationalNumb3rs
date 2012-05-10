89d14521894187f14cf232c5ca4120aeb08dbb19
Semo <semox78@gmail.com>
2012-05-10 16:34:38 +0200

    added new Functions.

--- rationalnumberarray.cpp
+++ rationalnumberarray.cpp
@@ -1,8 +1,9 @@
 #include "rationalnumberarray.h"
 #include "rationalnumber.h"
-#include <cstdlib>
+
 
 using namespace rnum;
+using namespace std;
 
 RationalNumberArray::RationalNumberArray(){
     RATIONALNUMBERARRAY_DEFAULT_SIZE = 12;
@@ -15,7 +16,6 @@ RationalNumberArray::RationalNumberArray(unsigned int cap){
 
 /**
   * Destructor
-  *
   */
 RationalNumberArray::~RationalNumberArray(){
     delete[] rn;
@@ -23,11 +23,56 @@ RationalNumberArray::~RationalNumberArray(){
 
 /**
   * Copy Constructor
-  *
   */
 RationalNumberArray::RationalNumberArray(const RationalNumberArray& r){
-    RationalNumberArray* rna = new RationalNumberArray(r.size);
     for (unsigned int i = 0; i < r.size; ++i) {
-        rna->rn[i] = r.rn[i];
+        this->rn[i] = r.rn[i];
     }
 }
+
+/**
+  * Return the amount of RationalNumbers a RationalNumberArray contains.
+  */
+unsigned int RationalNumberArray::arraySize(){
+    return this->size;
+}
+
+/**
+  * Returns the possible amount of RationalNumbers which can be stored inside.
+  */
+unsigned int RationalNumberArray::arrayCapacity(){
+    return this->capacity;
+}
+
+/**
+  * Removes RationalNumbers regarding the correct order in a RationalNumberArray.
+  */
+void RationalNumberArray::removeElements(const unsigned int from, const unsigned int to){
+    if (from < to) {
+        if(!(to <= this->arraySize ())){
+            cout << "index out of bounds" << endl;
+        } else {
+            int offset = to - from;
+            this->capacity -= offset;
+
+            for (unsigned int i = from; i <= this->arraySize(); ++i) {
+                if(i+offset < this->arraySize()) {
+                    this->rn[i] = this->rn[i+offset];
+                }
+            }
+            this->size -= offset;
+        }
+    }
+}
+
+///**
+//  * Adds a RationalNumber to the RationalNumberArray
+//  */
+//void RationalNumberArray::addElements(const RationalNumber& rn){
+//        if((arraySize(this) + 1) > arrayCapacity())  {
+//            arrayResize(rna, 2 * arrayCapacity(rna) + 2);
+//        }
+
+//        rna->rn[arraySize(rna)] = *rn;
+//        rna->size += 1;
+//}
--- rationalnumberarray.h
+++ rationalnumberarray.h
@@ -2,28 +2,38 @@
 #define RATIONALNUMBERARRAY_H
 
 #include "rationalnumber.h"
+#include <iostream>
+
 
 namespace rnum {
 class RationalNumberArray {
 
 public:
-    //Standard Constructor
+    // Standard Constructor
     RationalNumberArray();
 
-    //Constructor
+    // Constructor
     RationalNumberArray(unsigned int cap);
 
-    //Copy Constructor
-   RationalNumberArray(const RationalNumberArray& r);
+    // Copy Constructor
+    RationalNumberArray(const RationalNumberArray& r);
 
-    //Destructor
+    // Destructor
     ~RationalNumberArray();
 
-    //    unsigned int rnaSize();
-    //    unsigned int rnaCapacity();
-    //    void rnaRemove(const unsigned int from, const unsigned int to);
+    // Get amount of elements contained in a List
+    unsigned int arraySize();
+
+    // Returns the possible amount of RationalNumbers which can be stored inside.
+    unsigned int arrayCapacity();
+
+    // Removes RationalNumbers regarding the correct order in a RationalNumberArray.
+    void removeElements(const unsigned int from, const unsigned int to);
+
+    // Adds a RationalNumber to the RationalNumberArray
+//    void addElements(const RationalNumber& rn);
+
     //    void rnaResize(const unsigned int elements);
-    //    void rnaAdd(const RationalNumber& rn);
     //    void rnaSet(const RationalNumber& rn, const unsigned int index);
     //    RationalNumber rnaGet(const unsigned int index);
     //    void rnaPrint();
